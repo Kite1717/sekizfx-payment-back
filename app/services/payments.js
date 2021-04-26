64ff726b959648d88b0cb1ed4d0489f70183d0d6
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2021-04-27 02:32:18 +0300

    asdfsdsdf

--- app/services/payments.js
+++ app/services/payments.js
@@ -167,7 +167,7 @@ app.post("/accept-payment", async (req, res) => {
   //     return res.status(500).json({ err, msg: "DB error", status: 0 });
   //   });
 
-  db.Data.create({ data: JSON.stringify(req.body.data) })
+  db.Data.create({ data: JSON.stringify(req.body.data[0]) })
     .then(() => {
       return res.json({
         status: 1,
